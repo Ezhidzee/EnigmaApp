a4bd7869ccc7c0092ca3f2169512b47e9509ee77
Artem <atrem2004atrem@gmail.com>
2024-05-15 20:42:00 +0300

    Added SwipeToDelete for chats fragment TODO: connect that function to server, cuz now chats are deleted only locally

--- app/src/main/java/su/ezhidze/enigma/fragments/ChatsFragment.java
+++ app/src/main/java/su/ezhidze/enigma/fragments/ChatsFragment.java
@@ -1,15 +1,31 @@
 package su.ezhidze.enigma.fragments;
 
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
 import android.content.Intent;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.snackbar.Snackbar;
+
+import su.ezhidze.enigma.R;
 import su.ezhidze.enigma.activities.ConversationActivity;
 import su.ezhidze.enigma.activities.MainActivity;
 import su.ezhidze.enigma.activities.UsersActivity;
@@ -58,6 +74,7 @@ public class ChatsFragment extends Fragment implements RecentConversationChatLis
         setClickListeners();
         binding.recentConversationUsersRecyclerView.setVisibility(View.VISIBLE);
         binding.progressBar.setVisibility(View.GONE);
+        setSwipeToDelete();
     }
 
     private void setClickListeners() {
@@ -82,5 +99,81 @@ public class ChatsFragment extends Fragment implements RecentConversationChatLis
     public static void updateData() {
         chatList = chatManager.getChats();
         conversationUsersAdapter.updateChatList(chatList);
+    }
+
+    private void setSwipeToDelete() {
+        new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
+            @Override
+            public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+                return false;
+            }
+
+            @Override
+            public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+                Chat chat = chatList.get(viewHolder.getBindingAdapterPosition());
+
+                int position = viewHolder.getBindingAdapterPosition();
+                chatList.remove(position);
+                conversationUsersAdapter.updateChatList(chatList);
+
+                Snackbar.make(binding.recentConversationUsersRecyclerView, String.valueOf(chat.getId()), Snackbar.LENGTH_LONG).setAction("Undo", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        chatList.add(position, chat);
+                        conversationUsersAdapter.updateChatList(chatList);
+                    }
+                }).addCallback(new Snackbar.Callback(){
+                    @Override
+                    public void onDismissed(Snackbar transientBottomBar, int event) {
+                        super.onDismissed(transientBottomBar, event);
+                        Log.v(TAG, "Snackbar dismissed!");
+                    }
+                }).show();
+            }
+
+            @Override
+            public float getSwipeThreshold(@NonNull RecyclerView.ViewHolder viewHolder) {
+                return 1f;
+            }
+
+            @Override
+            public void onChildDraw(@NonNull Canvas c, @NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
+                setDeleteIcon(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+                super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+            }
+        }).attachToRecyclerView(binding.recentConversationUsersRecyclerView);
+    }
+
+    private void setDeleteIcon(Canvas c, RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
+        Paint mClearPaint = new Paint();
+        mClearPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.CLEAR));
+        ColorDrawable mBackground = new ColorDrawable();
+        int backgroundColor = Color.parseColor("#b80f0a");
+        Drawable deleteDrawable = ContextCompat.getDrawable(getActivity(), R.drawable.baseline_delete_24);
+        int intrinsicWidth = deleteDrawable.getIntrinsicWidth();
+        int intrinsicHeight = deleteDrawable.getIntrinsicHeight();
+
+        View itemView = viewHolder.itemView;
+        int itemHeight = itemView.getHeight();
+
+        boolean isCancelled = dX == 0 && !isCurrentlyActive;
+
+        if (isCancelled) {
+            c.drawRect(itemView.getRight() + dX, (float) itemView.getTop(), (float) itemView.getRight(), (float) itemView.getBottom(), mClearPaint);
+            return;
+        }
+
+        mBackground.setColor(backgroundColor);
+        mBackground.setBounds(itemView.getRight() + (int) dX, itemView.getTop(), itemView.getRight(), itemView.getBottom());
+        mBackground.draw(c);
+
+        int deleteIconTop = itemView.getTop() + (itemHeight - intrinsicHeight) / 2;
+        int deleteIconMargin = (itemHeight - intrinsicHeight) / 2;
+        int deleteIconLeft = itemView.getRight() - deleteIconMargin - intrinsicWidth;
+        int deleteIconRight = itemView.getRight() - deleteIconMargin;
+        int deleteIconBottom = deleteIconTop + intrinsicHeight;
+
+        deleteDrawable.setBounds(deleteIconLeft, deleteIconTop, deleteIconRight, deleteIconBottom);
+        deleteDrawable.draw(c);
     }
 }
